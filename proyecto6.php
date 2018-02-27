c5005a08b2ccdccf19af026d0ce42e7dec370448
alexiznieto <alexisnieto59@gmail.com>
2018-02-27 11:22:56 -0500

    nuevo archivo

---
+++ proyecto6.php
@@ -0,0 +1,13 @@
+<html>
+      <body>
+	  <h1> Ejercicio que calcula la definitiva de una materia con cuatro notas:</h1>
+	      
+		  <form action= "proyecto7.php" METHO="POST">
+		  Introduzca su nombre:
+		  <INPUT TYPE= "Text"NAME="nombre"><br>
+		  Introduzca nota 1:
+		  <INPUT TYPE= "Text"NAME="nota 1"><br>
+		  Introduzca nota 2:
+		  <INPUT TYPE= "Text"NAME="nota 2"><br>
+		  Introduzca nota 3:
+		  <INPUT TYPE= "Text"NAME="nota 3"><br>
